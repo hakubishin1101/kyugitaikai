1f8efd5a4ead596422218d4aeab6728f1e81924f
hakubishin1101 <johnouchi.contact@gmail.com>
2025-02-23 11:39:29 +0900

    初回コミット

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import QuestionForm from "./QuestionForm"; // 追加
+
+function App() {
+  return (
+    <div className="App">
+      <h2>質問フォーム</h2>
+      <QuestionForm />
+    </div>
+  );
+}
+
+export default App;
